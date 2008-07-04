c2f029c60b41d3c9a22093db20fdfefd18f3f679
lorenzo.tavernese <lorenzo.tavernese@d510715b-0851-0410-b22d-d588f1dd31d4>
2008-07-04 16:45:29 +0000

    aggiunte alcune cose a service. Mancano ancora le domande di servizio per le schede di rete

--- src/service.c
+++ src/service.c
@@ -16,6 +16,11 @@ int Overhead(double m)
  return (NDatagrams(m)*(TCPOV+IPOV+FRAMEOV)); //[Byte]
 }
 
+//calcola il numero di blocchi da leggere per un documento
+int number_of_blocks(double docSize)
+{
+ return ceil(docSize/BLOCK_SIZE);
+}
 
 //Calcola il tempo necessario per trasferire m Byte (sec)
 //m in byte, bandwidth in bit
@@ -27,23 +32,16 @@ double NetworkTime(double m, double bandwidth)
 //Domanda di servizio (sec), dove prendo AvgSizeHTTPRequest e Bandwidth? Di L1 non ho nessuna informazione
 double D_InLink()
 {
- return 0;
- //return NetworkTime(AvgSizeHTTPRequest, LinkBandwidth) + 2*NetworkTime(0.0001, LinkBandwidth)
+ return NetworkTime(AvgSizeHTTPRequest, LinkBandwidth) + 3*NetworkTime(0.0001, LinkBandwidth)
 }
 
 //manca LinkBandwidth
 double D_OutLink(double docSize)
 {
- return 0;
- //return NetworkTime(docSize, LinkBandwidth) + 2 * NetworkTime(0.0001, LinkBandwidth) 
+ return NetworkTime(docSize, LinkBandwidth) + 2 * NetworkTime(0.0001, LinkBandwidth) 
 }
 
-//Restituisce la domanda di servizio richiesta allo switch (posso modellarlo come un router????) Manca SwitchLatency (sec)
-double D_Switch(double docSize)
-{
- return 0
- //return (NDatagrams(1024*docSize)+6)* pow(SwitchLatency, -6);
-}
+
 /*Calcola la domanda di servizio sottomessa alla LAN che collega i Web Server con il File Server.
   Ogni richiesta attraversa la LAN per 2 volte: 
   1: comunicazione router -> Web Server (pacchetto HTTP standard)
@@ -54,8 +52,14 @@ double D_LAN(double docSize)
  return NetworkTime(AvgSizeHTTPRequest, BANDWIDTH_L2) + NetworkTime(1024 * docSize, BANDWIDTH_L2); //forse moltiplicare per un fattore 2 il secondo Network Time??? BOH
 }
 
-//Calcola la domanda di servizio sottomessa ad un Web Server (sec)
-double D_WSCpu(double docSize);
+//Calcola la domanda di servizio sottomessa ad una CPU (sia Web Switch che web server) (sec), fattore 2 perchè la comunicazione è two way ??? e nel caso con la modifica???
+double D_WebSwitchCpu()
+{
+  return 2 / CPU_WEB_SWITCH_SPEED;
+}
 
 //Calcola la domanda di servizio sottomessa ad un disco del Web Server (sec)
-double D_WSDisk(double docSize);
+double D_WSDisk(double docSize)
+{
+ NBlocks(docSize) * (DISK_SEEK_TIME + ROTATIONAL_LATENCY + CONTROLLER_TIME + (BLOCK_SIZE/DISK_TRANSFER_RATE));
+}
--- src/service.h
+++ src/service.h
@@ -8,6 +8,16 @@
 #define MSS 1460
 //#define N
 //#define S
+#define BLOCK_SIZE 2048 //dimensione in byte
+#define MBYTE 1048576  //equivalente di un mega espresso in byte
+#define LAMBDA 150 //richieste/sec  da parte degli utenti
+#define CPU_SERVICE_RATE 150 //richieste/secondo tasso servizio CPU
+#define DISK_SEEK_TIME 8.5 //millisecondi
+#define CONTROLLER_TIME 0,2 //millisecondi
+#define ROTATIONAL_SPEED 7200 //RPM
+#define DISK_REVOLUTION_TIME 60/ROTATIONAL_SPEED // sec
+#define ROTATIONAL_LATENCY 0.5*DISK_REVOLUTION_TIME //sec 
+#define DISK_TRANSFER_RATE 100 //MB/sec
 #define BANDWIDTH_L2 1024 //Mbps
 
 //Divide il numero di Byte m in ingresso in datagrammi
@@ -15,6 +25,9 @@ int NDatagrams(double m);
 
 //Calcola l'overhead della connessione TCP/IP e del livello DataLink ?????????
 int Overhead(double m);
+
+//calcola il numero di blocchi da leggere per un documento
+int number_of_blocks(double docSize);
 
 //Calcola il tempo necessario per trasferire m Byte (sec)
 //m in byte, bandwidth in bit
@@ -25,9 +38,6 @@ double D_InLink();
 
 double D_OutLink(double docSize);
 
-//Restituisce la domanda di servizio richiesta al router (sec)
-double D_Router(double docSize);
-
 /*Calcola la domanda di servizio sottomessa alla LAN che collega i Web Server con il File Server.
   Ogni richiesta attraversa la LAN per 2 volte: 
   1: comunicazione router -> Web Server (pacchetto HTTP standard)
@@ -35,8 +45,8 @@ double D_Router(double docSize);
 */
 double D_LAN(double docSize);
 
-//Calcola la domanda di servizio sottomessa ad un Web Server (sec)
-double D_WSCpu(double docSize);
+//Calcola la domanda di servizio sottomessa ad una CPU (sia Web Switch che web server) (sec)
+double D_Cpu(double speed);
 
 //Calcola la domanda di servizio sottomessa ad un disco del Web Server (sec)
 double D_WSDisk(double docSize);
