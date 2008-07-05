6a9ed794a6af76487dfb94904cb6419b727d503e
lorenzo.tavernese <lorenzo.tavernese@d510715b-0851-0410-b22d-d588f1dd31d4>
2008-07-05 13:30:17 +0000

    bozza del client, prima non avevo fatto l'add :)

---
+++ src/client.h
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <csim.h>
+#include "common.h"
+#include "gaussiana_inversa.h"
+#include "service.h"
+
+FACILITY cpuWS[NUM_SERVER];
+FACILITY diskWS[NUM_DISK];
+//FACILITY WS[NUM_SERVER];
+FACILITY L2;
+FACILITY CPU_web_switch;
+FACILITY inLink;
+FACILITY outLink;
+TABLE wsrtime;
+TABLE rtime;
+
+int web_client(double document_size);
+
+void web_session(int client_id, int variant);
