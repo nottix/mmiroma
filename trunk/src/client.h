30e07adc01a5c90fa7a4211a823a006ead87b930
lorenzo.tavernese <lorenzo.tavernese@d510715b-0851-0410-b22d-d588f1dd31d4>
2008-07-06 09:04:02 +0000

    continuata la bozza del client. non ci sto capendo una mazza

--- trunk/src/client.h
+++ trunk/src/client.h
@@ -16,6 +16,16 @@ FACILITY outLink;
 TABLE wsrtime;
 TABLE rtime;
 
+double mu_session = 3.86;
+double lambda_session = 9.46;
+double alfa_tt = 1.4;
+double alfa_obj = 1.33;
+double mu_html = 7.63;
+double sigma_html = 1.001;
+double alfa_html = 1;
+double mu_emb = 8.215;
+double sigma_emb = 1.46;
+
 int web_client(double document_size);
 
-void web_session(int client_id, int variant);
+void web_session(int cli_id, int variant);
