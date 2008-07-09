d359ea772a4d823e950bc086ef2f246079b076a9
lorenzo.tavernese <lorenzo.tavernese@d510715b-0851-0410-b22d-d588f1dd31d4>
2008-07-09 13:00:01 +0000

    corretti alcuni errori

--- trunk/src/gaussiana_inversa.h
+++ trunk/src/gaussiana_inversa.h
@@ -3,13 +3,13 @@
 
 double calc_mean_lognorma(double mu, double sigma) ;
 
-double calc_stddev_lognormal(double mu, ouble sigma);
+double calc_stddev_lognormal(double mu, double sigma);
 
 int session_request(double mu, double lambda);   //dimensionare come un intero???
 
 double user_think_time(double alfa);
 
-double object_per_request(double alfa);
+int object_per_request(double alfa);
 
 double html_page_size(double mu, double sigma, double alfa);
 
