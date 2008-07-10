2b5c99c78ac3bd1c71944a789c7c8b4422a23f1d
lorenzo.tavernese <lorenzo.tavernese@d510715b-0851-0410-b22d-d588f1dd31d4>
2008-07-10 13:27:54 +0000

    prima prova di clustering e correzione di alcuni errori

--- trunk/src/cluster.c
+++ trunk/src/cluster.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
 #include<string.h>
 #include<csim.h>
 #include "gaussiana_inversa.h"
@@ -12,8 +15,8 @@ extern double alfa_tt;
 extern double alfa_obj;
 extern double mu_html;
 extern double sigma_html;
-extern double alfa_htm;
-extern double mu_emb = ;
+extern double alfa_html;
+extern double mu_emb;
 extern double sigma_emb;
 
 extern STREAM sess_req_1;
@@ -22,7 +25,7 @@ extern STREAM user_tt;
 extern STREAM object_req;
 extern STREAM html_1;
 extern STREAM html_2;
-extern STREAM obj_size
+extern STREAM obj_size;
 
 void cluster(double *array, int array_length, int k)
 {
@@ -32,13 +35,13 @@ void cluster(double *array, int array_length, int k)
   int counter = 0;
   int converged = 0;
   double distance;
+  double distance_temp;
   double min = array[0];
-  printf("array[0]: %lf\n", array[0]);
   double max = array[0];
-  double centroids[k];
-  double old_centroids[k];
-  double num_data_per_cluster[k];
-  double total_data_per_cluster[k];
+  double *centroids = (double*)malloc(sizeof(double)*k) ;
+  double *old_centroids = (double*)malloc(sizeof(double)*k);
+  long *num_data_per_cluster = (long*)malloc(sizeof(long)*k);
+  double *total_data_per_cluster = (double*)malloc(sizeof(double)*k);
   //calcolo del minimo e del massimo
   for(i=1;i < array_length; i++) {
      if(array[i] > max)
@@ -57,16 +60,23 @@ void cluster(double *array, int array_length, int k)
     		centroids[i] = (max-min)/(k-1) * i;
   	}
   }
- 
+  printf("centroide medio %lf\n", centroids[1]);
   while(!iter) {
+	for(i=0; i < k; i++) {
+	 printf("centroid temp %lf\n", centroids[i]); 
+   } 
     //assegnazione dei dati ad ogni cluster
   	for(i=0; i < array_length; i++) {
 			distance = fabs(centroids[0] - array[i]);
+		
     	for(j=1; j < k; j++) {
-			  double distance_temp = fabs(centroids[j]-array[i]);
+
+			  distance_temp = fabs(centroids[j]-array[i]);
 				if(distance > distance_temp) {
 					distance = distance_temp;
 					counter = j;
+					
+				
 				}
 			}
 			num_data_per_cluster[counter] += 1;
@@ -82,12 +92,9 @@ void cluster(double *array, int array_length, int k)
 		printf("\n");
 		//aggiornamento dei centroidi in seguito al primo step e confronto con la precedente iterazione
 		for(i=0; i < k; i++) {
-		  if(num_data_per_cluster[i] != 0) { //prova, in realtà non dovrebbe mai accadere che un centroide resti vuoto
-				centroids[i] = total_data_per_cluster[i] / num_data_per_cluster[i]; 
-		  }
-		  else
-		  	centroids[i] = 0;
-		  printf("centroide: %lf - tot_dati: %lf\t", centroids[i], num_data_per_cluster[i]);
+
+			centroids[i] = total_data_per_cluster[i] / ((double)num_data_per_cluster[i]); 
+		  printf("centroide: %f - tot_dati: %ld\t", centroids[i], num_data_per_cluster[i]);
 			if( fabs(centroids[i]-old_centroids[i]) < pow(10,-10) ) {  //valore epsilon = 10^-15
 				converged++;
 			}
@@ -103,9 +110,9 @@ void cluster(double *array, int array_length, int k)
 void sim(int argc, char **argv)
 {
 	sess_req_1 = create_stream();
-	reseed(sess_req1, SEED);
+	reseed(sess_req_1, SEED);
 	sess_req_2 = create_stream();
-	reseed(sess_req2, SEED);
+	reseed(sess_req_2, SEED);
 	user_tt = create_stream();
 	reseed(user_tt, SEED);
 	object_req = create_stream();
@@ -118,7 +125,7 @@ void sim(int argc, char **argv)
 	reseed(obj_size, SEED);
 	
 	create("prova");
-	int array_length= 1000000;
+	int array_length= 10000000;
 	double *array = (double*)malloc(sizeof(double)*array_length);
 	double session;
 	int objects;
@@ -140,6 +147,15 @@ void sim(int argc, char **argv)
 			}
 		}
 	}
+	int m=0;
+	FILE *fd_file;
+	char *pathname = (char*)malloc(128);
+  sprintf(pathname, "docs.txt");
+   fd_file = fopen(pathname, "w");
+	for(m=0;m<array_length;m++) {
+	fprintf(fd_file, "%lf\n", array[m]);
+	}
+	fclose(fd_file); 
 	cluster(array,array_length,3);
 }
 
