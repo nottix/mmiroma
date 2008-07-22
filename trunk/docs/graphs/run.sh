4954ca259f8ef19534beaf9b0d34399cc84ee111
notargiacomo.s <notargiacomo.s@d510715b-0851-0410-b22d-d588f1dd31d4>
2008-07-22 14:05:57 +0000

    fix

--- trunk/docs/graphs/run.sh
+++ trunk/docs/graphs/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #gnuplot script
 
-XLB="Intervallo migrazione (sec)"
-YLB="Tempo impiegato (sec)"
+XLB="Intervallo"
+YLB="Cambia"
 
 if [ "$1" = "" ]; then
 	echo "Usage: run.sh <source_data>"
@@ -12,10 +12,10 @@ fi
 echo $1
 echo "set size 1.5" > aux.gnu
 echo "set output \"$1.eps\"" >> aux.gnu
-echo "set title \"Test\"" >> aux.gnu
+#echo "set title \"Test\"" >> aux.gnu
 echo "set xlabel \"$XLB\"" >> aux.gnu
 echo "set ylabel \"$YLB\"" >> aux.gnu
-echo "plot \"$1\" with linespoints pointtype 2 pointsize 3 lt 1 linewidth 8" >> aux.gnu
+echo "plot \"$1\" notitle with lines lt 1 linewidth 1" >> aux.gnu
 gnuplot script.input
 wait
 epstopdf $1.eps
